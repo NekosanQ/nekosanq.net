d0265e2a36b43b69a071bc4295de5f060625069b
NekosanQ <NekosanQ@outlook.com>
2025-05-04 19:27:53 +0900

    add: 背景の追加/記述法の変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-// App.tsx
 import React from 'react';
 import Navbar from './components/Navbar';
+import StarBackground from './components/StarBackground';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black/[95%] text-white relative overflow-hidden">
+      <StarBackground />
       <Navbar />
-      <main className="pt-16 px-4 text-white"></main>
+      <main className="pt-16 px-4 text-white relative z-10"></main>
     </div>
   );
 };
---
+++ src/components/StarBackground.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StarBackground: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+};
+
+export default StarBackground;
