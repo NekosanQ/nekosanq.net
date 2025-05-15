987b6e1febe9231cf3a472e5387d7b4f1dfc44d5
NekosanQ <NekosanQ@outlook.com>
2025-05-15 12:57:16 +0900

    feat: テキストの位置とスマホレスポンシブに対応

--- src/components/BigText.tsx
+++ src/components/BigText.tsx
@@ -5,8 +5,13 @@ import { ReactTyped } from 'react-typed';
 
 const BigText: React.FC = () => {
   return (
-    <div className="text-center text-white text-5xl mt-40 font-caveat">
-      <ReactTyped strings={["I'm NekosanQ"]} typeSpeed={50} showCursor={false} contentType="html" />
+    <div className="text-center md:text-left text-white text-5xl mt-40 ml-0 md:ml-96 font-bold">
+      <ReactTyped
+        strings={["I'm NekosanQ"]}
+        typeSpeed={50}
+        showCursor={false}
+        contentType="html"
+      />
     </div>
   );
 };
