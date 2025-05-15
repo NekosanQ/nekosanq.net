c41ee1a977e7094c31b23100ff402eb9f4f1bd48
NekosanQ <NekosanQ@outlook.com>
2025-05-15 15:12:25 +0900

    feat: ソーシャルメディアリンクを追加

--- src/components/BigText.tsx
+++ src/components/BigText.tsx
@@ -1,30 +1,33 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactTyped } from 'react-typed';
+import SocialLink from './SocialMediaLink';
 
 const BigText: React.FC = () => {
+  const [isTypedDone, setIsTypedDone] = useState(false);
+
   return (
     <div className="text-center md:text-left mt-40 ml-0 md:ml-96 font-bold relative">
-      <h1
-        className="text-5xl bg-gradient-to-t from-slate-200 via-green-200 to-slate-200 bg-clip-text text-transparent
-                   drop-shadow-[0_0_5px_rgba(255,255,255,0.6)]">
-        <ReactTyped
-          strings={["I'm NekosanQ"]}
-          typeSpeed={50}
-          showCursor={false}
-          contentType="html"
-        />
+      <h1 className="text-5xl bg-gradient-to-t from-slate-200 via-lime-100 to-slate-200 bg-clip-text text-transparent drop-shadow-[0_0_5px_rgba(255,255,255,0.6)]">
+        <ReactTyped strings={["I'm NekosanQ"]} typeSpeed={50} showCursor={false} contentType="html" />
       </h1>
 
-      <p className="text-lg bg-gradient-to-t from-gray-300 via-gray-500 to-gray-300 bg-clip-text text-transparent mt-4 ">
+      <p className={'text-lg text-gray-400 mt-4'}>
         <ReactTyped
-          strings={["A developer and a student with many hobbies..."]}
-          typeSpeed={50}
+          strings={['A developer and a student with many hobbies...']}
+          typeSpeed={30}
           startDelay={1000}
           showCursor={false}
+          onComplete={() => setIsTypedDone(true)}
         />
       </p>
+
+      {isTypedDone && (
+        <div className="mt-6 transition-opacity duration-1000 ease-out opacity-100">
+          <SocialLink />
+        </div>
+      )}
     </div>
   );
 };
