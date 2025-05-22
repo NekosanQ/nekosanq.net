74e6789f5a37dec32433b72d2bbf78a5909cf4b4
NekosanQ <NekosanQ@outlook.com>
2025-05-22 17:12:17 +0900

    feat: metaタグの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import './global.css';
 import Navbar from '../components/Navbar';
 import { ReactNode } from 'react';
+import type { Viewport } from 'next';
 
 export const metadata = {
   title: 'NekosanQ - Portfolio',
   description: 'NekosnQのポートフォリオサイトです。',
-  keywords: ['NeksaanQ', 'Nekonnection', 'Portfolio'],
+  keywords: ['NeksaanQ', 'Nekonnection', 'Portfolio']
 };
 
+export const viewport: Viewport = {
+  themeColor: '#90ee90',
+};
 interface RootLayoutProps {
   children: ReactNode;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ const Home = () => {
       <div className="fixed inset-0 z-0">
         <StarBackground />
       </div>
+
       {/* メインコンテンツ */}
       <main className="relative z-10">
         <section id="home" className="relative min-h-screen">
@@ -37,7 +38,6 @@ const Home = () => {
         {/* About Me セクション */}
         <section id="about" className="relative flex flex-col items-center justify-center md:py-32 md:-mt-96 space-y-20 max-w-7xl mx-auto px-4">
           <SectionHeader title="About me" />
-
           <div className="flex flex-col lg:flex-row items-center justify-center gap-12 w-full">
             <AboutMeCard />
             <StacksCard />
@@ -50,6 +50,7 @@ const Home = () => {
           <SectionHeader title="Nekonnection Service" />
           <Service />
         </section>
+
         {/* フッター */}
         <Footer />
       </main>
